bee3106d3503337d599097a07b17835c73bbde32
Prajwal Tiwari <prajwal@prajwal.tiwari>
2024-02-24 16:04:13 +0530

    Add comments in exceptionTest file

--- cypress/e2e/ExceptionHandling/exceptionTest.cy.js
+++ cypress/e2e/ExceptionHandling/exceptionTest.cy.js
@@ -1,35 +1,65 @@
 import Exception from "../../support/Pages/Exception/testExceptions.page";
 const ex = new Exception();
 
+/**
+ * @description Test Suite For Exception Handling
+ */
 describe('Test Suite For Exception Handling',()=>{
     let creds;
+
+    /**
+     * @description Before each test, load test data
+     */
     before(()=>{
         cy.fixture('testData.json').then((data) => {
             creds = data;
           });
     })
+
+    /**
+     * @description Before each test, load exception page
+     */
     beforeEach(()=>{
         ex.ExceptionsPageLoad()
     })
+
+    /**
+     * @description Test case 1: NoSuchElementException handling
+     */
     it('Test case 1: NoSuchElementException handling',()=>{
         const Inputtexts = creds.api;
-        ex.addNewRowAndVerifyTwoInput(Inputtexts.newName) //aded proper wait in base class
+        ex.addNewRowAndVerifyTwoInput(Inputtexts.newName) //added proper wait in base class
     })
+
+    /**
+     * @description Test case 2: ElementNotInteractableException handling
+     */
     it('Test case 2: ElementNotInteractableException handling',()=>{ //used indexing so that only the valid save button will be clicked
         const Inputtexts = creds.api;
-        ex.addNewRowAndVerifyTwoInput(Inputtexts.newName) //aded proper wait in base class
+        ex.addNewRowAndVerifyTwoInput(Inputtexts.newName) //added proper wait in base class
         ex.addDataInRow2(Inputtexts.newJob)
         ex.verifySaveSuccess('Row 2 was saved')
     })
+
+    /**
+     * @description Test case 3: Verify By default input field is disabled
+     */
     it('Test case 3 : Verify By default input field is disabled',()=>{
         ex.verifyInputDisabledbyDefault()
     })
-    // Test case 4: StaleElementReferenceException
+
+    /**
+     * @description Test case 4: StaleElementReferenceException
+     */
     it('Test case 4: StaleElementReferenceException handling',()=>{
         ex.clickAddButton()
         ex.verifySaveSuccess('Row 2 was added')
         ex.verifyAddButtonNotvisible()
     })
+
+    /**
+     * @description Test case 5: TimeoutException handling
+     */
     it('Test case 5: TimeoutException handling',()=>{
        ex.clickAddAndVerifyTwoInputRows()
     })
